d7fd34e03790d08720a10735f15e40f8ee7cd86c
Sakthi <89055838+Sakthi51@users.noreply.github.com>
2022-09-13 19:25:23 +0530

    Create visualizing_decision_trees.py

---
+++ visualizing_decision_trees.py
@@ -0,0 +1,17 @@
+from sklearn.tree import DecisionTreeClassifier
+import joblib
+from sklearn import tree
+
+music_data = pd.read_csv('music.csv')
+x = music_data.drop(columns=['genre'])
+y = music_data['genre']
+
+model = DecisionTreeClassifier()
+model.fit(x.values, y)
+
+tree.export_graphviz(model, out_file = 'music_recommender.dot',
+                    feature_names = ['age', 'gender'],
+                    class_names = sorted(y.unique()),
+                    label = 'all',
+                    rounded = True,
+                    filled = True)
